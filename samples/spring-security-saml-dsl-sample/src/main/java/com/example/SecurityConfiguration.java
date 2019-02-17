791f3d5ff73c6a27599090046bb327a0e23feaf8
Steven C. Saliman <support@saliman.net>
2019-02-17 12:56:40 -0700

    Added some tracom specifics to the sample app and publishing plugin

--- samples/spring-security-saml-dsl-sample/src/main/java/com/example/SecurityConfiguration.java
+++ samples/spring-security-saml-dsl-sample/src/main/java/com/example/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.opensaml.xml.signature.SignatureConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,12 +19,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		String signingAlgorithm = SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA256;
+		String signingDigest = SignatureConstants.ALGO_ID_DIGEST_SHA256;
+
 		http
 			.authorizeRequests()
 				.antMatchers("/saml/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.apply(saml())
+				.signingAlgorithm("RSA", signingAlgorithm, signingDigest)
 				.serviceProvider()
 					.keyStore()
 						.storeFilePath("saml/keystore.jks")
@@ -31,6 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 						.keyname("spring")
 						.keyPassword("secret")
 						.and()
+				.maxAuthenticationAge(86400)
 					.protocol("https")
 					.hostname("localhost:8443")
 					.basePath("/")
